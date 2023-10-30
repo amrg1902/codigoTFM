11a987ee903850ccc1f43a087ae389416904000c
amrg1902 <aitormrg@gmail.com>
2023-10-30 17:10:24 +0100

    Subida

--- CODIGO/despliegue-container.groovy
+++ CODIGO/despliegue-container.groovy
@@ -6,9 +6,19 @@ pipeline {
             steps {
                 script {
                     def nginxImage = 'nginx:latest' // Puedes cambiar la etiqueta de la imagen si es necesario
+                    // Iniciar un temporizador de 30 segundos (puedes ajustar el tiempo según tus necesidades)
+                    def timeoutSeconds = 30
+                    def startTime = currentBuild.startTimeInMillis
 
                     // Ejecuta el contenedor NGINX con Docker
                     sh "docker run -d -p 80:80 --name mi-contenedor-nginx $nginxImage"
+                    
+                    // Comprueba si ha pasado el tiempo especificado
+                    def elapsedTime = (currentBuild.startTimeInMillis - startTime) / 1000
+                    if (elapsedTime >= timeoutSeconds) {
+                        error "Tiempo de ejecución excedido. Cerrando la etapa."
+                    
+                    }
                 }
             }
         }
