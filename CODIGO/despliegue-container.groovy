ed3549e621922f3bceb817b7d81bc7cd712ed995
amrg1902 <aitormrg@gmail.com>
2023-11-01 11:29:08 +0100

    Ajusto direccion del dockerfile

--- CODIGO/despliegue-container.groovy
+++ CODIGO/despliegue-container.groovy
@@ -2,18 +2,9 @@ pipeline {
     agent any
 
     stages {
-        stage('Build Docker Image') {
+        stage('Mensaje de Éxito') {
             steps {
-                script {
-                    // Construir la imagen Docker
-                    def dockerfilePath = '/Users/aitormartin-romogonzalez/Documents/GitHub/AAA-TFM/CODIGO/'
-                    dir(dockerfilePath) {
-                        def customImage = docker.build('mi_imagen_mlflow:latest', '.')
-                    }
-                    customImage.withRegistry('https://index.docker.io/v1/', 'ce19e7ac-fe9e-47b6-aa1d-65aaf111e2d8') {
-                        customImage.push()
-                    }
-                }
+                echo '¡Clonado exitosamente desde GitHub!'
             }
         }
     }
