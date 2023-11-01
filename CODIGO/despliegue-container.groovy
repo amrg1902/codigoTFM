e69071ce7bd2643beadb23df0997c4550fc7bdb7
amrg1902 <aitormrg@gmail.com>
2023-11-01 11:59:17 +0100

    Ajusto direccion del dockerfile

--- CODIGO/despliegue-container.groovy
+++ CODIGO/despliegue-container.groovy
@@ -2,24 +2,12 @@ pipeline {
     agent any
 
     stages {
-        stage('Ejemplo de Acceso al Workspace') {
+        stage('Build Docker Image') {
             steps {
                 script {
-                    // Obtener la ruta al directorio de trabajo (workspace)
-                    def workspaceDir = env.WORKSPACE
-
-                    // Definir el nombre del archivo que deseas acceder
-                    def archivo = "${workspaceDir}/CODIGO/Dockerfile"
-
-                    // Mostrar un mensaje en el registro de Jenkins
-                    echo "Contenido del archivo ${archivo}:"
+                    sh "sudo docker build -t mlflow:latest ."
                     
                 }
-            }
-        }
-        stage('Mensaje de Éxito') {
-            steps {
-                echo '¡Clonado exitosamente desde GitHub!'
             }
         }
     }
