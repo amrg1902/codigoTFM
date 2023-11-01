0f944cf1834d6f308fcd5573755a515878ad9fbe
amrg1902 <aitormrg@gmail.com>
2023-11-01 11:24:38 +0100

    Ajusto direccion del dockerfile

--- CODIGO/despliegue-container.groovy
+++ CODIGO/despliegue-container.groovy
@@ -10,6 +10,8 @@ pipeline {
                     dir(dockerfilePath) {
                         def customImage = docker.build('mi_imagen_mlflow:latest', '.')
                     }
+                    customImage.withRegistry('https://index.docker.io/v1/', 'ce19e7ac-fe9e-47b6-aa1d-65aaf111e2d8') {
+                        customImage.push()
                 }
             }
         }
