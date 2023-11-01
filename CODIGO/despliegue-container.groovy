36bbf95aceda2023a368a8e0b2f0b13ebffa703d
amrg1902 <aitormrg@gmail.com>
2023-11-01 11:02:47 +0100

    Prueba MLFlow

--- CODIGO/despliegue-container.groovy
+++ CODIGO/despliegue-container.groovy
@@ -2,13 +2,24 @@ pipeline {
     agent any
 
     stages {
-        stage('Build Docker Image') {
+        stage('Clonar Repositorio') {
             steps {
                 script {
-                    // Construir la imagen Docker
-                    docker.build('mi_imagen_mlflow:latest', '-f Dockerfile .')
+                    // Definir la URL del repositorio de GitHub
+                    def repoURL = 'https://github.com/amrg1902/AAA-TFM.git'
+                    
+                    // Clonar el repositorio en la carpeta de trabajo actual
+                    checkout([$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [], userRemoteConfigs: [[url: repoURL]]])
                 }
             }
         }
+        // stage('Build Docker Image') {
+        //     steps {
+        //         script {
+        //             // Construir la imagen Docker
+        //             docker.build('mi_imagen_mlflow:latest', '-f Dockerfile .')
+        //         }
+        //     }
+        // }
     }
 }
