9429ff88f529ac0e34b5ecbd7b9d7a1b3272e4bf
amrg1902 <aitormrg@gmail.com>
2023-11-01 11:03:48 +0100

    Prueba MLFlow

--- CODIGO/despliegue-container.groovy
+++ CODIGO/despliegue-container.groovy
@@ -6,7 +6,7 @@ pipeline {
             steps {
                 script {
                     // Definir la URL del repositorio de GitHub
-                    def repoURL = 'https://github.com/amrg1902/AAA-TFM.git'
+                    def repoURL = 'git@github.com:amrg1902/AAA-TFM.git'
                     
                     // Clonar el repositorio en la carpeta de trabajo actual
                     checkout([$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [], userRemoteConfigs: [[url: repoURL]]])
