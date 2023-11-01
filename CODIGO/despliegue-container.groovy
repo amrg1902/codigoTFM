4145cf5eecab106d58d2182c5dfb0a30244e7022
amrg1902 <aitormrg@gmail.com>
2023-11-01 10:52:06 +0100

    Prueba MLFlow

--- CODIGO/despliegue-container.groovy
+++ CODIGO/despliegue-container.groovy
@@ -2,18 +2,12 @@ pipeline {
     agent any
 
     stages {
-        stage('Install MLflow') {
+        stage('Build Docker Image') {
             steps {
-                sh '/Users/aitormartin-romogonzalez/opt/anaconda3/bin/python -m venv venv'
-                sh 'source venv/bin/activate'
-                sh '/Users/aitormartin-romogonzalez/opt/anaconda3/bin/pip install mlflow'
-            }
-        }
-
-
-        stage('Run MLflow Server') {
-            steps {
-                sh 'mlflow server --host 0.0.0.0 --port 5000'
+                script {
+                    // Construir la imagen Docker
+                    docker.build('mi_imagen_mlflow:latest', '-f Dockerfile .')
+                }
             }
         }
     }
