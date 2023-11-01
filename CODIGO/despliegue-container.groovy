446250860569ba494e61e55fb4a07121fd810947
amrg1902 <aitormrg@gmail.com>
2023-11-01 11:39:57 +0100

    Ajusto direccion del dockerfile

--- CODIGO/despliegue-container.groovy
+++ CODIGO/despliegue-container.groovy
@@ -14,14 +14,7 @@ pipeline {
                     // Construir la ruta completa al archivo en el workspace
                     def rutaCompleta = "${workspaceDir}/${archivo}"
 
-                    // Verificar si el archivo existe en el workspace
-                    if (fileExists(rutaCompleta)) {
-                        echo "El archivo ${archivo} existe en el workspace."
-                        
-                        // Realizar acciones adicionales con el archivo
-                    } else {
-                        echo "El archivo ${archivo} no se encontró en el workspace."
-                    }
+                    sh "cat ${rutaDockerfile}"
                 }
             }
         }
