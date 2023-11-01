77ac9b127dd5582316d5e8035b1613ff5d5380c1
amrg1902 <aitormrg@gmail.com>
2023-11-01 12:18:39 +0100

    Prueba directorio PATH

--- CODIGO/despliegue-container.groovy
+++ CODIGO/despliegue-container.groovy
@@ -2,11 +2,23 @@ pipeline {
     agent any
 
     stages {
-        stage('Build Docker Image') {
+        stage('Check PATH') {
             steps {
                 script {
-                    sh "docker build -t mlflow:latest ."
+                    // Obtenemos el valor de la variable PATH
+                    def path = sh(script: 'echo $PATH', returnStdout: true).trim()
                     
+                    // Imprimimos el contenido de la variable PATH
+                    echo "Contenido de la variable PATH:"
+                    echo path
+                    
+                    // Puedes agregar condiciones o comprobar valores específicos en la variable PATH aquí
+                    // Por ejemplo, verificar si una ruta específica está presente
+                    if (path.contains('/ruta/especifica')) {
+                        echo "La ruta /ruta/especifica está presente en PATH."
+                    } else {
+                        error "La ruta /ruta/especifica no está presente en PATH."
+                    }
                 }
             }
         }
