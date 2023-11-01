8b593f6f5dff817d1b76e6963facc5535adb434e
amrg1902 <aitormrg@gmail.com>
2023-11-01 10:29:35 +0100

    Prueba MLFlow

--- CODIGO/despliegue-container.groovy
+++ CODIGO/despliegue-container.groovy
@@ -2,12 +2,14 @@ pipeline {
     agent any
 
     stages {
-        
         stage('Install MLflow') {
             steps {
+                sh 'python -m venv venv'
+                sh 'source venv/bin/activate'
                 sh 'pip install mlflow'
             }
         }
+
 
         stage('Run MLflow Server') {
             steps {
