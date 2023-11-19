953af0d89d6b9eb3c3173217ede7a49d81948023
amrg1902 <aitormrg@gmail.com>
2023-11-19 14:49:36 +0100

    Intento de adaptación codigo

--- model-training/train_model.py
+++ model-training/train_model.py
@@ -34,11 +34,14 @@
 # Importa las bibliotecas necesarias
 import mlflow
 import mlflow.sklearn
+from mlflow import MlflowClient
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+#Log into MLFlow
+client = MlflowClient()
 # Carga el conjunto de datos Breast Cancer Wisconsin
 data = load_breast_cancer()
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
