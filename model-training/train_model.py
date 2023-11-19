9046f11c7ccff6dbfb878d19770b8ee588309beb
amrg1902 <aitormrg@gmail.com>
2023-11-19 15:05:33 +0100

    Intento de adaptación codigo

--- model-training/train_model.py
+++ model-training/train_model.py
@@ -40,8 +40,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-#Log into MLFlow
-client = MlflowClient()
+
 # Carga el conjunto de datos Breast Cancer Wisconsin
 data = load_breast_cancer()
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
@@ -57,6 +56,9 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy}')
 
+
+#Log into MLFlow
+client = MlflowClient()
 # Configura la URI de seguimiento de MLflow
 mlflow.set_tracking_uri("http://mlflow_container:80")
 mlflow.set_experiment('Entrenamiento de prueba mas complejo')
