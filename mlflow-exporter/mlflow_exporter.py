6bf1cad4b4cadeb03c13355805edbd0cab4d3f20
amrg1902 <aitormrg@gmail.com>
2023-11-20 16:52:47 +0100

    Adapto el exporter para que coja todos los experimentos

--- mlflow-exporter/mlflow_exporter.py
+++ mlflow-exporter/mlflow_exporter.py
@@ -14,11 +14,14 @@ mlflow.set_tracking_uri("http://mlflow_container:80")
 @app.route('/metrics') #Para que prometheus los raspe correctamente
 def mostrar_experimentos():
 
-    # Nombre del experimento
-    nombre_experimento = "Entrenamiento de prueba mas complejo"
+    # # Nombre del experimento
+    # nombre_experimento = "Entrenamiento de prueba mas complejo"
 
-    # Obtén el ID del experimento por su nombre
-    experimento_id = mlflow.get_experiment_by_name(nombre_experimento).experiment_id
+    # Obtiene la lista de todos los experimentos
+    experimentos = mlflow.search_runs().experiment_name.unique()
+
+    ## Obtén el ID del experimento por su nombre
+    #experimento_id = mlflow.get_experiment_by_name(nombre_experimento).experiment_id
 
     # Obtén todas las ejecuciones del experimento
     runs = mlflow.search_runs(experiment_ids=experimento_id)
@@ -26,14 +29,29 @@ def mostrar_experimentos():
     # Inicializa metricas_prometheus
     metricas_prometheus = ""
 
-    # Itera sobre las ejecuciones y muestra las métricas
-    for index, run in runs.iterrows():
-        run_id = run.run_id
-        metrics = mlflow.get_run(run_id).data.metrics
-        for metric_name, metric_value in metrics.items():
-            metricas_prometheus += f'{metric_name}{{run_id="{run_id}"}} {metric_value}\n'
-        print(f"Metrics for run {run_id}: {metrics}")
+    # # Itera sobre las ejecuciones y muestra las métricas
+    # for index, run in runs.iterrows():
+    #     run_id = run.run_id
+    #     metrics = mlflow.get_run(run_id).data.metrics
+    #     for metric_name, metric_value in metrics.items():
+    #         metricas_prometheus += f'{metric_name}{{run_id="{run_id}"}} {metric_value}\n'
+    #     print(f"Metrics for run {run_id}: {metrics}")
 
+    # Itera sobre los experimentos
+    for nombre_experimento in experimentos:
+        # Obtén el ID del experimento por su nombre
+        experimento_id = mlflow.get_experiment_by_name(nombre_experimento).experiment_id
+
+        # Obtén todas las ejecuciones del experimento
+        runs = mlflow.search_runs(experiment_ids=experimento_id)
+
+        # Itera sobre las ejecuciones y muestra las métricas
+        for index, run in runs.iterrows():
+            run_id = run.run_id
+            metrics = mlflow.get_run(run_id).data.metrics
+            for metric_name, metric_value in metrics.items():
+                metricas_prometheus += f'{metric_name}{{experiment="{nombre_experimento}", run_id="{run_id}"}} {metric_value}\n'
+            print(f"Metrics for run {run_id} in experiment {nombre_experimento}: {metrics}")
 
     # Renderiza la plantilla HTML con la lista de experimentos
     response = make_response(metricas_prometheus)
