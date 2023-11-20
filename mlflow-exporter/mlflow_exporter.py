58028a002c5d59e4b161cdf9e0543896179c7e8a
amrg1902 <aitormrg@gmail.com>
2023-11-20 14:50:37 +0100

    Prueba de obtener datos de mlflow

--- mlflow-exporter/mlflow_exporter.py
+++ mlflow-exporter/mlflow_exporter.py
@@ -33,4 +33,4 @@ def mostrar_experimentos():
     return render_template('experimentos.html', experimentos=experimentos)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
