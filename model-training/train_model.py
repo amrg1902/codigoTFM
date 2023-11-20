01abf5299943cd3c6af203d8a9b3f03b3b2d1693
amrg1902 <aitormrg@gmail.com>
2023-11-20 16:41:46 +0100

    Pruebo a meter otro modelo

--- model-training/train_model.py
+++ model-training/train_model.py
@@ -1,37 +1,4 @@
 # Importa las bibliotecas necesarias
-# import os
-# import mlflow
-# import mlflow.sklearn
-# from sklearn.datasets import load_iris
-# from sklearn.model_selection import train_test_split
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import accuracy_score
-
-# Configura la URI de la base de datos y la dirección del servidor de MLflow
-# mlflow.set_tracking_uri("http://mlflow_container:80")
-# mlflow.set_experiment('Entrenamiento prueba data iris')
-
-# # Carga los datos de iris
-# iris = load_iris()
-# X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
-
-# # Entrenamiento del modelo
-# model = RandomForestClassifier(n_estimators=100, random_state=42)
-# model.fit(X_train, y_train)
-
-# # Realiza predicciones en el conjunto de prueba
-# y_pred = model.predict(X_test)
-
-# # Calcula la precisión del modelo
-# accuracy = accuracy_score(y_test, y_pred)
-
-# # Log en MLflow
-# with mlflow.start_run():
-#     mlflow.log_param("n_estimators", 100)
-#     mlflow.log_metric("accuracy", accuracy)
-#     mlflow.sklearn.log_model(model, "model")
-
-# Importa las bibliotecas necesarias
 import mlflow
 import mlflow.sklearn
 from sklearn.datasets import load_breast_cancer
