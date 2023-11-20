88047f192387d54d731db62654f5b54395d01dde
amrg1902 <aitormrg@gmail.com>
2023-11-20 14:45:34 +0100

    Prueba de obtener datos de mlflow

--- mlflow-exporter/mlflow_exporter.py
+++ mlflow-exporter/mlflow_exporter.py
@@ -7,30 +7,30 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
 
 # Configura la URI de seguimiento de MLflow
 mlflow.set_tracking_uri("http://mlflow_container:80")
-mlflow.set_experiment('Entrenamiento de prueba mas complejo')
 
-@metrics.summary('mlflow_accuracy', 'MLflow accuracy')
-@app.route('/train_model')
-def train_model():
-    data = load_breast_cancer()
-    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
+#@metrics.summary('mlflow_accuracy', 'MLflow accuracy')
+#@app.route('/train_model')
+# def train_model():
+#     with mlflow.start_run():
+#         mlflow.log_param("n_estimators", 100)
+#         mlflow.log_metric("accuracy", accuracy)
+#         mlflow.sklearn.log_model(model, "model")
 
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
+#     return f"Model trained with accuracy: {accuracy}"
 
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8000)
 
-    with mlflow.start_run():
-        mlflow.log_param("n_estimators", 100)
-        mlflow.log_metric("accuracy", accuracy)
-        mlflow.sklearn.log_model(model, "model")
+@app.route('/')
+def mostrar_experimentos():
+    # Obtiene la lista de experimentos
+    experimentos = mlflow.search_runs().experiment_name.unique()
 
-    return f"Model trained with accuracy: {accuracy}"
+    # Renderiza la plantilla HTML con la lista de experimentos
+    return render_template('experimentos.html', experimentos=experimentos)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(debug=True)
