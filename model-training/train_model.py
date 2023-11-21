0fdfebc9930b0db8a64240ef2ef2bbe0b64550fc
amrg1902 <aitormrg@gmail.com>
2023-11-21 15:26:40 +0100

    Introduzco un segundo entrenamiento en el primer modelo

--- model-training/train_model.py
+++ model-training/train_model.py
@@ -5,6 +5,7 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 
 # Carga el conjunto de datos Breast Cancer Wisconsin
 data = load_breast_cancer()
