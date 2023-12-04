84b7378bd9122f1a7f164aeccdeea64ecb293e64
amrg1902 <aitormrg@gmail.com>
2023-12-04 18:33:46 +0100

    Prueba nuevas metricas

--- model-training2/train_model.py
+++ model-training2/train_model.py
@@ -7,10 +7,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-#Configura la URI de la base de datos y la dirección del servidor de MLflow
-mlflow.set_tracking_uri("http://mlflow_container:80")
-mlflow.set_experiment('Entrenamiento prueba data diabetes')
 
 # Cargar el conjunto de datos
 data_diabetes = load_diabetes()
@@ -44,19 +43,30 @@ X_train, X_test, y_train, y_test = train_test_split(selected_data, y_diabetes, t
 
 ############################################# Random Forest ###################################
 # Entrenamiento del modelo
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+RFCmodel = RandomForestClassifier(n_estimators=100, random_state=42)
+RFCmodel.fit(X_train, y_train)
 # Realiza predicciones en el conjunto de prueba
-y_pred = model.predict(X_test)
-# Calcula la precisión del modelo
-accuracy = accuracy_score(y_test, y_pred)
+y_pred = RFCmodel.predict(X_test)
+# Redondear las predicciones
+y_pred = pd.Series(y_pred, name='target')
+
+###METRICAS
+mse = mean_squared_error(y_test, y_pred)
+rmse = mean_squared_error(y_test, y_pred, squared=False)
+r2 = r2_score(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+
 # Definir el nombre del run
 run_name = "Random Forest Classifier"
 # Log en MLflow
 with mlflow.start_run(run_name=run_name):
     mlflow.log_param("n_estimators", 100)
-    mlflow.log_metric("accuracy", accuracy)
-    mlflow.sklearn.log_model(model, "model")
+    # Log de métricas
+    mlflow.log_metric("RFC_mse", mse)
+    mlflow.log_metric("RFC_rmse", rmse)
+    mlflow.log_metric("RFC_r2", r2)
+    mlflow.log_metric("RFC_mae", mae)
+    mlflow.sklearn.log_model(RFCmodel, "RandomForestClassifier_model")
 
 ################################################ SVM #############################################
 # Entrenamiento del modelo SVM
@@ -64,12 +74,24 @@ svm_model = SVC(random_state=42)
 svm_model.fit(X_train, y_train)
 # Realiza predicciones en el conjunto de prueba
 y_pred_svm = svm_model.predict(X_test)
-# Calcula la precisión del modelo SVM
+# Redondear las predicciones
+y_pred_svm = pd.Series(y_pred_svm, name='target')
+
+# Calcula métricas
 accuracy_svm = accuracy_score(y_test, y_pred_svm)
+precision_svm = precision_score(y_test, y_pred_svm, average='weighted')
+recall_svm = recall_score(y_test, y_pred_svm, average='weighted')
+f1_svm = f1_score(y_test, y_pred_svm, average='weighted')
+
 # Definir el nombre del run
 run_name2 = "SVM"
 # Log en MLflow
 with mlflow.start_run(run_name=run_name2):
     mlflow.log_param("random_state", 42)
-    mlflow.log_metric("accuracy", accuracy_svm)
-    mlflow.sklearn.log_model(model, "svm_model")
+    # Log de métricas
+    mlflow.log_metric("SVM_accuracy", accuracy_svm)
+    mlflow.log_metric("SVM_precision", precision_svm)
+    mlflow.log_metric("SVM_recall", recall_svm)
+    mlflow.log_metric("SVM_f1_score", f1_svm)
+
+    mlflow.sklearn.log_model(svm_model, "svm_model")
