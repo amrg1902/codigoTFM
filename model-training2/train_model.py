fd38d1dd090e16e3768ae693b2df2225c5788895
amrg1902 <aitormrg@gmail.com>
2023-12-04 18:47:18 +0100

    Prueba nuevas metricas

--- model-training2/train_model.py
+++ model-training2/train_model.py
@@ -18,7 +18,7 @@ from sklearn.preprocessing import StandardScaler
 
 #Configura la URI de la base de datos y la dirección del servidor de MLflow
 mlflow.set_tracking_uri("http://mlflow_container:80")
-mlflow.set_experiment("Entrenamiento prueba data diabetes")
+mlflow.set_experiment("Entrenamiento dataset diabetes")
 
 # Cargar el conjunto de datos
 data_diabetes = load_diabetes()
