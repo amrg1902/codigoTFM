7c13bb117237d20d247b5df3506e901a3707659f
amrg1902 <aitormrg@gmail.com>
2023-12-11 13:56:58 +0100

    Pruebas

--- api/api.py
+++ api/api.py
@@ -40,59 +40,60 @@ def fetch_model(model_name):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Obtén el mejor modelo y su métrica asociada
+    best_model_name = fetch_best_model()
+    best_model_metric = None
 
-@app.route('/predict', methods=['GET'])
-def model_output():
-    try:
-        age = float(request.args.get('age'))
-        bmi = float(request.args.get('bmi'))
-        bp = float(request.args.get('bp'))
-        s1 = float(request.args.get('s1'))
-        s2 = float(request.args.get('s2'))
-        s3 = float(request.args.get('s3'))
-        s4 = float(request.args.get('s4'))
-        s5 = float(request.args.get('s5'))
-        s6 = float(request.args.get('s6'))
+    if best_model_name:
+        # Obtén el valor de la métrica (MSE) del mejor modelo
+        experimento_id = mlflow.get_experiment_by_name(nombre_experimento).experiment_id
+        best_model_run = mlflow.search_runs(experiment_ids=experimento_id, filter_string=f"run_name = '{best_model_name}'").iloc[0]
+        best_model_metric = best_model_run["MSE"]
 
-    # model_name = fetch_best_model()
-    # model = fetch_model(model_name)
+    return render_template('index.html', best_model=best_model_name, best_model_metric=best_model_metric)
 
-    # input_data = pd.DataFrame({"age": [age], "bmi": [bmi], "bp": [bp], "s1": [s1], "s2": [s2], "s3": [s3], "s4": [s4], "s5": [s5], "s6": [s6]})
-    # prediction = model.predict(input_data)
+@app.route('/model_details')
+def model_details():
+    # Obtén el mejor modelo y carga los detalles del modelo
+    best_model_name = fetch_best_model()
+    model = fetch_model(best_model_name)
 
-    # return render_template('index.html', prediction=f"Prediction: {prediction[0]}")
-        model_name = fetch_best_model()
-        if model_name:
-            model = fetch_model(model_name)
-            print(model)
-            if model:
-                input_data = pd.DataFrame({"age": [age], "bmi": [bmi], "bp": [bp], "s1": [s1], "s2": [s2], "s3": [s3], "s4": [s4], "s5": [s5], "s6": [s6]})
-                prediction = model.predict(input_data)
-                print(prediction)
-                return render_template('index.html', prediction=f"Prediction: {prediction[0]}")
-            else:
-                return render_template('index.html', prediction="Error: Model not loaded")
-        else:
-            return render_template('index.html', prediction="Error: No best model found")
-    except Exception as e:
-        return render_template('index.html', prediction=f"Error: {str(e)}")
+    return render_template('model_details.html', best_model=best_model_name, model_details=model)
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/predict', methods=['GET'])
+# def model_output():
+#     try:
+#         age = float(request.args.get('age'))
+#         bmi = float(request.args.get('bmi'))
+#         bp = float(request.args.get('bp'))
+#         s1 = float(request.args.get('s1'))
+#         s2 = float(request.args.get('s2'))
+#         s3 = float(request.args.get('s3'))
+#         s4 = float(request.args.get('s4'))
+#         s5 = float(request.args.get('s5'))
+#         s6 = float(request.args.get('s6'))
+
+#         model_name = fetch_best_model()
+#         if model_name:
+#             model = fetch_model(model_name)
+#             print(model)
+#             if model:
+#                 input_data = pd.DataFrame({"age": [age], "bmi": [bmi], "bp": [bp], "s1": [s1], "s2": [s2], "s3": [s3], "s4": [s4], "s5": [s5], "s6": [s6]})
+#                 prediction = model.predict(input_data)
+#                 print(prediction)
+#                 return render_template('index.html', prediction=f"Prediction: {prediction[0]}")
+#             else:
+#                 return render_template('index.html', prediction="Error: Model not loaded")
+#         else:
+#             return render_template('index.html', prediction="Error: No best model found")
+#     except Exception as e:
+#         return render_template('index.html', prediction=f"Error: {str(e)}")
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=7654)
-
-
-#@app.route('/predict')
-# def model_output(age: float, bmi: float, bp: float, s1: float, s2: float, s3: float, s4: float, s5: float, s6: float):
-#     print("Works I")
-#     model_name = fetch_best_model()
-#     model = fetch_model(model_name)
-#     print("Works II")
-#     input = pd.DataFrame({"age": [age], "bmi": [bmi], "bp": [bp], "s1": [s1], "s2": [s2], "s3": [s3], "s4": [s4], "s5": [s5], "s6": [s6]})
-#     prediction = model.predict(input)
-#     print(prediction)
-#     return {"prediction": prediction[0]}
-
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=7654)   
